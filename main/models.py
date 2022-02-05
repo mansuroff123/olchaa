7ff73cec813990c394a8d2600ad258d46ee8644b
hojiakbar <mansuroff145@gmail.com>
2022-02-05 17:42:15 +0500

    deploy4

---
+++ main/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class SmsCode(models.Model):
+    phone = models.CharField(max_length=16, db_index=True)
+    ip = models.GenericIPAddressField(db_index=True)
+    code = models.CharField(max_length=10)
+    expire_at = models.DateTimeField(db_index=True)
+
+    class Meta:
+        index_together = []
+
+
+class SmsAttempt(models.Model):
+    phone = models.CharField(max_length=16, db_index=True)
+    counter = models.IntegerField(default=0)
+    last_attempt_at = models.DateTimeField(db_index=True)
